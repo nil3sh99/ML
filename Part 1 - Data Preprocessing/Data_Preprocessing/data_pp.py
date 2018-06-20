8df24c660c01e1df148f3044bbab73e07101dcc3
Nilesh <agnilesh8199@gmail.com>
2018-06-20 19:30:08 +0530

    Reading datasets

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
@@ -1,5 +1,9 @@
-import pandas as pd
 import numpy as np
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
+import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[:,:-1 ] #reading all rows and columns except last clumn
+Y = dataset.iloc[:,3] #reading only the column which has index 3(in this case it is the last column)
+
