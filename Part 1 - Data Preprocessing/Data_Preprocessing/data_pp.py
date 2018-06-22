e2c7b6506c988c23844ead49d89851d552bbc0a4
Nilesh <agnilesh8199@gmail.com>
2018-06-23 01:22:45 +0530

    adding variables

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
@@ -27,3 +27,4 @@ y = labelencoder_y.fit_transform(y)
 
 from sklearn.cross_validation import train_test_split
 # after above stmt is depricated use this--> sklearn.model_selection.train_test_split()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =0.2 )
