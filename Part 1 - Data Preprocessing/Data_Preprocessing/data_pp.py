7a81dbd8784aba929c7ebeff62f6cd347003cf94
Nilesh <agnilesh8199@gmail.com>
2018-06-23 01:06:35 +0530

    categorising data according to index number

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
@@ -14,6 +14,9 @@ imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.categorical_features(X).toarray()
+
