baf7d997d6174edf163100e93582969fb854ad3c
Nilesh <agnilesh8199@gmail.com>
2018-06-25 00:56:50 +0530

    training and test set - setup

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_pp.py
@@ -1,3 +1,5 @@
+# Data Preprocessing
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,17 +16,15 @@ imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
-#dummy variable addition
-X = onehotencoder.categorical_features(X).toarray()
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-
-#splitting dataset into training and test set
-
-from sklearn.cross_validation import train_test_split
-# after above stmt is depricated use this--> sklearn.model_selection.train_test_split()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =0.2 )
+#training set = on which we build our machine learning model
+#test set = on which we will test our model
