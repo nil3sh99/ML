dc7ec5f5ea0952087a8943f27b3a086f84e11dc2
Nilesh <agnilesh8199@gmail.com>
2018-07-02 14:23:10 +0530

    Update and rename error.py to predictionError.py

--- College_work/predictionError.py
+++ College_work/predictionError.py
@@ -1,8 +1,10 @@
+
 from sklearn import metrics
 
 y_true = [3,3,2,1,2]
 
 y_pred = [2,2,2,3,3]
+#Input i.e. Actual or True & Predicted
 
 print(metrics.mean_absolute_error(y_true, y_pred))
 print(metrics.mean_squared_error(y_true, y_pred))
