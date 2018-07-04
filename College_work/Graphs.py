4d8e78fa4a7d4bc6523ad06dd26ea2975cbe5778
Nilesh <agnilesh8199@gmail.com>
2018-07-04 13:47:40 +0530

    Updating Graphs

--- College_work/Graphs.py
+++ College_work/Graphs.py
@@ -1,19 +1,45 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+x=[1,2,3,4,5]
+y=[7,8,3,2,1]
+z=[2,5,6,1,8]
+#y=[1,2,3,4,5]
+#z=[1,2,3,4,5]
 
-x = [1,2,3,4,5]
-y = [1,2,3,4,5]
-z = [5,6,7,8,9]
+plt.xlabel('x->')
+plt.ylabel('y->')
+plt.title('graph')
+plt.xlim(0,20)
+plt.ylim(0,20)
+plt.plot(x,y,'--r',label='Sine Curve')
+plt.plot(x,z,':b',label='Graph 2')
+plt.legend(loc='upper center')
+plt.show()
+'''
+'''
+plt.scatter(x,y,s=100,c='red',marker='x')
+plt.show()
+'''
+'''
+plt.bar(x,y, color="green")
+plt.show()
+'''
+'''
+label=['Delhi','Mumbai','Calcutta', 'Chennai']
+slices=[10,20,30,40]
+plt.pie(slices,labels=label,explode=(0,0,0.1,0))
+plt.legend()
+plt.show()
 
-fig = plt.figure()
 
-plt1 = fig.add_subplot(231)
-plt1.set_xlim(0,10)
-plt1.set_ylim(0,10)
-plt1.set_title('Graph:')
-plt1.set_xlabel('X values -->')
-plt1.set_ylabel('Y values -->')
-plt1.plot(x,y, '--g', label = 'City Index')
-plt1.plot(x,z, '-r', label = 'expenditureIndex')
-'''plt1.legend()'''
-plt1.show()
+fig=plt.figure()
+plt1=fig.add_subplot(111,projection='3d')
+plt1.scatter(x,y,z,s=100,c='r')
+plt.show()
+
