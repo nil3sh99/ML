cda724097d50f63ea7f8b78fc8accdf29dccce26
Nilesh <agnilesh8199@gmail.com>
2018-07-08 01:18:07 +0530

    firing up the libraries

---
+++ Part 2- Regression/simple linear regression/simpleLinearRegression.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  8 01:04:43 2018
+
+@author: nilesh
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+
