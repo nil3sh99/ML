4c0f558908a0d4477e944321905c058472d609f4
Nilesh <agnilesh8199@gmail.com>
2018-07-08 01:18:56 +0530

    adding comments

--- Part 2- Regression/simple linear regression/simpleLinearRegression.py
+++ Part 2- Regression/simple linear regression/simpleLinearRegression.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1].values #taking in all the vlaues except the last column
+y = dataset.iloc[:, 1].values # fetchin the independent variable values
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
