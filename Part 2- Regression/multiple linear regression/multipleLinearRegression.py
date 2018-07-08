005e9633fe3a249081b7cbdfa61baf3a839d31df
Nilesh <agnilesh8199@gmail.com>
2018-07-08 14:32:35 +0530

    spliting dataset

--- Part 2- Regression/multiple linear regression/multipleLinearRegression.py
+++ Part 2- Regression/multiple linear regression/multipleLinearRegression.py
@@ -12,6 +12,6 @@ dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
-
+# spliting the dataset
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
