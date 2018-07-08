4c5a8dceb1438dc6d25974f6432fc5d76b74dd55
Nilesh <agnilesh8199@gmail.com>
2018-07-08 14:54:48 +0530

    Fitting the dataset using linear Regression library

--- Part 2- Regression/multiple linear regression/multipleLinearRegression.py
+++ Part 2- Regression/multiple linear regression/multipleLinearRegression.py
@@ -34,3 +34,7 @@ X = X[:, 1:]
 # spliting the dataset
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
