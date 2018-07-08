ce1c6d99f8867ab0fff96be3fdd4e8b3910db9c7
Nilesh <agnilesh8199@gmail.com>
2018-07-08 14:43:40 +0530

    Conerting the categorical data to integer data

--- Part 2- Regression/multiple linear regression/multipleLinearRegression.py
+++ Part 2- Regression/multiple linear regression/multipleLinearRegression.py
@@ -12,6 +12,12 @@ dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
 # spliting the dataset
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
