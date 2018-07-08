ed7385372778cdf1d4e789d765b847f4e929622f
Nilesh <agnilesh8199@gmail.com>
2018-07-08 14:29:55 +0530

    Stating the problem

---
+++ Part 2- Regression/multiple linear regression/multipleLinearRegression.py
@@ -0,0 +1,15 @@
+#we have to  make our model predict from the independent variable to 
+# predict the profit column i.e our dependent variable
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("50_Startups.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
