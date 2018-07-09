08ffbffd135df6f1949f367e3a417637131423ee
Nilesh <agnilesh8199@gmail.com>
2018-07-09 10:19:24 +0530

    Enabling feature scaling for proper working of SVR model

--- Part 2- Regression/Simple Vector Regression/SVR.py
+++ Part 2- Regression/Simple Vector Regression/SVR.py
@@ -13,3 +13,25 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+# applying feature scaling to the dataset
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+# fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+
+#preicting the new result
+y_pred = regressor.predict(6.5)
+
+# Visualising the SVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth OR Bluff (SVR)')
+plt.xlabel('Postion level')
+plt.ylabel('Salary')
+plt.show()
