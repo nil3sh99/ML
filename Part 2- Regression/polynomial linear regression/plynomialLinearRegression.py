88bc46f23f4b436afc55ed6dc8a206730e17b772
Nilesh <agnilesh8199@gmail.com>
2018-07-09 07:53:46 +0530

    Removing the training and testing && Feature Scaling part

--- Part 2- Regression/polynomial linear regression/plynomialLinearRegression.py
+++ Part 2- Regression/polynomial linear regression/plynomialLinearRegression.py
@@ -14,6 +14,6 @@ dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
+#from sklearn.cross_validation import train_test_split
+#X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
 
