20dd93d56f1f327d4e00998907ccf9fd6e0a36bc
Nilesh <agnilesh8199@gmail.com>
2018-07-09 22:01:17 +0530

    Decision Tree template

---
+++ Part 2- Regression/Decision Tree/decisionTree.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 21:51:54 2018
+
+@author: nilesh
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:None].values
+
+# Fitting the Decision Tree Regression to the dataset
+
+
+
+# Predicting a new result
+y_pred = regressor.predict(6.5)
+
+# Visualizing the Decision Tree Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth of Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
