76d192c8f94a2d99979a87556c3adfd261ced6d7
Nilesh <agnilesh8199@gmail.com>
2018-07-09 07:52:24 +0530

    Setting up the polynomial linear regression file

--- Part 2- Regression/multiple linear regression/multipleLinearRegression.py
+++ Part 2- Regression/multiple linear regression/multipleLinearRegression.py
@@ -70,4 +70,4 @@ regressor_OLS.summary()
 
 X_opt = X[:, [0,3]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
-regressor_OLS.summary()
+regressor_OLS.summary() 
---
+++ Part 2- Regression/polynomial linear regression/plynomialLinearRegression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 07:40:57 2018
+
+@author: nilesh
+"""
+#importing libs
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state =0)
+
