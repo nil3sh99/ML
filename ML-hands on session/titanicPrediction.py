09c986261112bcbd981ab910fbaa273837c9b368
Nilesh <agnilesh8199@gmail.com>
2018-07-13 10:32:42 +0530

    Setting up

---
+++ ML-hands on session/titanicPrediction.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 10:26:12 2018
+
+@author: nilesh
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("train.csv")
