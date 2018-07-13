ea553af4234c4a9c397da9a5cb50b5dd33cedcd0
Nilesh <agnilesh8199@gmail.com>
2018-07-13 10:49:40 +0530

    Printing few rows of train and test data

--- ML-hands on session/titanicPrediction.py
+++ ML-hands on session/titanicPrediction.py
@@ -10,4 +10,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv("train.csv")
+#dataset = pd.read_csv("train.csv")
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+train.head(5)
