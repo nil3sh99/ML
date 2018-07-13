f9207d4b7bdd7aea6d6bd7925bd01d13c9dead25
Nilesh <agnilesh8199@gmail.com>
2018-07-13 11:11:45 +0530

    visualizing casulaties

--- ML-hands on session/titanicPrediction.py
+++ ML-hands on session/titanicPrediction.py
@@ -9,7 +9,7 @@ Created on Fri Jul 13 10:26:12 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+%matplotlib inline
 #dataset = pd.read_csv("train.csv")
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
@@ -25,3 +25,15 @@ test.info()
 
 train.isnull().sum()# this tells how many data are absent for the train dataset
 test.isnull().sum()# this tells how many data are absent for the test dataset
+
+import seaborn as sns
+sns.set() #setting seaborn default for plot
+
+def bar_chart(feature):
+    survived = train[train['Survived']==1][feature].value_counts()
+    dead =train[train['Survived']==0][feature].value_counts()
+    df = pd.DataFrame([survived,dead])
+    df.index = ['survived', 'dead']
+    df.plot(kind = 'bar',stacked = True, figsize=(10,5))
+
+bar_chart('Parch')
