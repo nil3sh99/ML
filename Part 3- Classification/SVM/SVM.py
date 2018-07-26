f98a1d4bc357c45848269d0e1d283c252af928d9
Nilesh <agnilesh8199@gmail.com>
2018-07-26 16:31:29 +0530

    Firing up the SVM

--- Part 3- Classification/SVM/SVM.py
+++ Part 3- Classification/SVM/SVM.py
@@ -6,3 +6,6 @@ Created on Thu Jul 26 15:55:15 2018
 @author: nilesh
 """
 
+# SVM works on the rule that,  a line is drawn in between the data points
+# and the distance from the adjacent Support vectors is calculated
+# That line/plane is selected which has the maximum margin from the support vector points
