3a07bd79ecdc2a821909d2ed9e1c6d9ae31f7823
Nilesh <agnilesh8199@gmail.com>
2018-08-01 15:59:45 +0530

    Loading dataset

---
+++ Projects/project1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  1 15:47:06 2018
+
+@author: nilesh
+"""
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+from sklearn import model_selection
+from sklearn.metrics import classification_report
