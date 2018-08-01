dce82863ab6a5475914288058bd376b8ea258760
Nilesh <agnilesh8199@gmail.com>
2018-08-01 16:14:13 +0530

    Done importing libraries

--- Projects/project1.py
+++ Projects/project1.py
@@ -19,4 +19,3 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-
