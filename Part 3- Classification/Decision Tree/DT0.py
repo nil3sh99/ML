f52701617103750c452e64241cf7016c35aa898a
Nilesh <agnilesh8199@gmail.com>
2018-08-02 23:27:44 +0530

    setting up graph params

--- Part 3- Classification/Decision Tree/DT0.py
+++ Part 3- Classification/Decision Tree/DT0.py
@@ -56,7 +56,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('KNN')
+plt.title('Decision Tree (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
