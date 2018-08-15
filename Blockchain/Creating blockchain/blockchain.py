ead98fe478cc77ddf6d004cb3125ae4807937efe
Nilesh <agnilesh8199@gmail.com>
2018-08-16 02:36:10 +0530

    Creating blockchain

---
+++ Blockchain/Creating blockchain/blockchain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 02:21:03 2018
+
+@author: nilesh
+"""
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
