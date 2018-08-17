1f90c8cb3ba4f5f00d8943b07435896ad0a452d6
Nilesh <agnilesh8199@gmail.com>
2018-08-17 22:22:44 +0530

    creating the blockchain function

--- Blockchain/Creating blockchain/blockchain.py
+++ Blockchain/Creating blockchain/blockchain.py
@@ -25,3 +25,17 @@ class blockchain:
         self.chain = [] # chain containing the chains --> list
         self.create_block(proof = 1, preHash= '0')
                                     #SHA256 can only use encoded STRINGS and hence we took it as a string
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) +1,
+                 'timeStamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previousHash':previous_hash
+                 # we can add any data to the blockchain by adding any key here
+                 }
+        self.chain.append(block) # chain is a list and hence we used its append function
+        return block
+        
+        
+        
+        
+        
