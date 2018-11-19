5f08a684248f53ec9da7293e3e462e1f5a94f151
Nilesh <agnilesh8199@gmail.com>
2018-11-20 02:01:44 +0530

    Tensor flow project 1

---
+++ tensorFlow/tf1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 01:39:40 2018
+
+@author: Nilesh
+"""
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
